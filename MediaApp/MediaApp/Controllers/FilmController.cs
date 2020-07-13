ac240f1a485a88fc9e1fb410eb373d399b5a0208
Jolien De Letter <joliendeletter@gmail.com>
2020-07-13 09:49:54 +0200

    Added mediacontroller

--- MediaApp/MediaApp/Controllers/FilmController.cs
+++ MediaApp/MediaApp/Controllers/FilmController.cs
@@ -19,13 +19,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MediaApp.Controllers
 {
-    public class FilmController : Controller
+    public class FilmController : MediaController
     {
         private readonly MediaDb _dbContext;
         private readonly IPhotoService _photoService;
         private readonly SignInManager<User> _signInManager;
 
-        public FilmController(MediaDb dbcontext, IPhotoService photoService, SignInManager<User> signInManager)
+        public FilmController(MediaDb dbcontext, IPhotoService photoService, SignInManager<User> signInManager) : base(dbcontext)
         {
             _dbContext = dbcontext;
             _photoService = photoService;
@@ -122,7 +122,7 @@ namespace MediaApp.Controllers
                 Duration = film.Duration,
                 ContentUrl = film.ContentUrl,
                 Public = film.Public,
-                Comments = await _dbContext.Comments.Include(x => x.User).Where(comment => comment.MediaId == film.Id).OrderByDescending(x => x.PublishedDate).ToListAsync()
+                Comments = await _dbContext.Comments.Include(x => x.User).Where(comment => comment.MediaId == film.Id && comment.IsApproved == true).OrderByDescending(x => x.PublishedDate).ToListAsync()
             };
 
             return View(vm);
@@ -273,34 +273,6 @@ namespace MediaApp.Controllers
             }
 
             return (RedirectToAction("Index"));
-        }
-        public async Task RemoveFromSeen(int id)
-        {
-            User user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
-
-            MediaSeen mediaSeen = await _dbContext.MediaSeens.Where(x => x.MediaId == id && x.UserId == user.Id).FirstOrDefaultAsync();
-
-            _dbContext.MediaSeens.Remove(mediaSeen);
-            await _dbContext.SaveChangesAsync();
-        }
-
-        public async Task AddToSeen(int id)
-        {
-            User user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
-            _dbContext.MediaSeens.Add(new MediaSeen() { MediaId = id, UserId = user.Id });
-            await _dbContext.SaveChangesAsync();
-        }
-        public async Task Comment(int Id, string comment)
-        {
-            _dbContext.Comments.Add(new Comment()
-            {
-                UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),
-                Description = comment,
-                PublishedDate = DateTime.Now,
-                MediaId = Id
-
-            });
-            await _dbContext.SaveChangesAsync();
         }
     }
 }
---
+++ MediaApp/MediaApp/Controllers/MediaController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using MediaApp.Domain;
+using MediaApp.Domain.MediaTypes;
+using MediaApp.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaApp.Controllers
+{
+    public class MediaController : Controller
+    {
+        private readonly MediaDb _dbContext;
+
+        public MediaController(MediaDb dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+        public async Task RemoveFromSeen(int id)
+        {
+            User user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            MediaSeen mediaSeen = await _dbContext.MediaSeens.Where(x => x.MediaId == id && x.UserId == user.Id).FirstOrDefaultAsync();
+
+            _dbContext.MediaSeens.Remove(mediaSeen);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task AddToSeen(int id)
+        {
+            User user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
+            _dbContext.MediaSeens.Add(new MediaSeen() { MediaId = id, UserId = user.Id });
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task Comment(int Id, string comment)
+        {
+            _dbContext.Comments.Add(new Comment()
+            {
+                UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),
+                Description = comment,
+                PublishedDate = DateTime.Now,
+                MediaId = Id
+
+            });
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
